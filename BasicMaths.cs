f0bdd1ce346a4fea40ff3721fed176905f4f96cd
anilkumar76609 <anilkumar76609@gmail.com>
2022-05-19 18:18:58 +0530

    initial commit

---
+++ BasicMaths.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BasicMathematicsproject
+{
+    public class BasicMaths
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+
+        }
+        public double substract(double num1, double num2)
+        {
+            return (num1 - num2);
+        }
+        public double divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+        public double multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+      
+    }
+}
+    
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BasicMathematicsproject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("BasicMathematicsproject")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9ecf548d-5529-450e-971a-d77600b86ae6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
